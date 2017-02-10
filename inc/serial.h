1564abef3a5004bb6e5629be58eef12ef0d79b78
Marko Pavlin <marko@pavlin.si>
2017-02-11 00:30:51 +0100

    Initial commit

---
+++ inc/serial.h
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  * File Name          : serial.h
+  * Description        : serial port io
+  ******************************************************************************
+  *
+  * Copyright (c) 2016 S54MTB
+  * Licensed under Apache License 2.0 
+  * http://www.apache.org/licenses/LICENSE-2.0.html
+  *
+  ******************************************************************************
+  */
+#ifndef __serial_h__
+#define __serial_h__
+
+
+void process_rx_char(char rx_char);
+void uart_puts(char *str);
+uint8_t IsLineReceived(void);
+char *get_line_buffer(void);
+
+
+
+#endif
+
+
